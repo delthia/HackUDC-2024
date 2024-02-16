cfa11a4325150df719223f8ddbfdbca364057bfc
Delthia <git@delthia.com>
2024-02-17 00:55:19 +0100

    Mejorar ruta de API de consumo

--- electrodatos/routes.py
+++ electrodatos/routes.py
@@ -1,7 +1,7 @@
 from electrodatos import app
 from electrodatos.models import Electrodata
-from flask import render_template
-import datetime
+from flask import render_template, request
+from datetime import datetime
 
 @app.route("/")
 def homepage():
@@ -16,16 +16,17 @@ def cliente(id_cliente):
     ids_clientes = list(range(1, 100))
     if id_cliente not in ids_clientes:
         return "Parece que no eres un cliente que exista"
+    return render_template("cliente.html", cliente = id_cliente)
     return f"Eres el cliente {id_cliente}"
 
-@app.route("/datos-ejemplo/<int:id_cliente>/<inicio>/<fin>")
-def datos_ejemplo(id_cliente, inicio, fin):
+@app.route("/api/consumo/rango")
+def datos_ejemplo():
     electrodatos = Electrodata("electrodatos/datos/electrodatos.csv")
-    inicio, fin = inicio.split('-'), fin.split('-')
-    series = electrodatos.cliente(id_cliente, datetime.datetime(int(inicio[0]), int(inicio[1]), int(inicio[2])), datetime.datetime(int(fin[0]), int(fin[1]), int(fin[2])))
+
+    id_cliente = request.args.get('cliente', type=int)
+    inicio, fin = datetime.fromisoformat(request.args.get('inicio', type=str)), datetime.fromisoformat(request.args.get('fin', type=str))
+    print(inicio, fin)
+
+    series = electrodatos.cliente(id_cliente, inicio, fin)
     cadena = f"var fechas = {series[0]};\nvar datos = {series[1]};"
     return cadena
-
-@app.route("/grafico")
-def grafico():
-    return render_template("graphjs.html")
